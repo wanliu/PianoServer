ec31ebc07412dc7a973e08f03015ed7edc750937
Hysios Hu <hysios@gmail.com>
2015-07-10 13:58:34 +0800

    fixed bug #2 user input can't display content

--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -6,10 +6,12 @@ class PromotionCollection < ActiveResource::Collection
 end
 
 class Promotion < ActiveResource::Base
+  UPYUN_SITES = %r(neil-img.b0.upaiyun.com)
+
   self.site = Settings.wanliu.backend
   self.collection_parser = PromotionCollection
 
   def image_mobile_url
-    image_url + '!moblie'
+    UPYUN_SITES =~ image_url ? image_url + '!moblie': image_url
   end
 end
