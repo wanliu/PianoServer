2c6b0bd2318b26a9bb7fba591f4addd179b9c563
liulei <satbirdd@gmail.com>
2015-07-21 11:38:36 +0800

    fix migrate error

--- db/migrate/20150716050531_change_items_data_hstore_to_json.rb
+++ db/migrate/20150716050531_change_items_data_hstore_to_json.rb
@@ -1,4 +1,4 @@
-class ChangeDataHstoreToJson < ActiveRecord::Migration
+class ChangeItemsDataHstoreToJson < ActiveRecord::Migration
   def change
     change_column :items, :data, 'jsonb USING CAST(data AS jsonb)', null: false, default: '{}'
   end
