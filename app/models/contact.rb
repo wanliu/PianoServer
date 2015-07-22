7b5ab09ca5b7d6443369e4ae6dcc6518d522b5c6
Billy.Zheng <zw963@163.com>
2015-07-22 16:44:09 +0800

    添加中文提示信息

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,4 @@
 class Contact < ActiveRecord::Base
-  validates_presence_of :name, :mobile, :message
+  validates_presence_of :name, :message, message: '不可以为空！'
+  validates :mobile, presence: {message: '请输入手机号码！'}, format: { with: /\A1[34578]\d{9}\z/, message: '手机号码格式不正确' }
 end
