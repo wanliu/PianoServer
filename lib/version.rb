9e802ef154ffeb6c358f0855633bc933ad1a8140
Hysios Hu <hysios@gmail.com>
2015-07-24 10:52:13 +0800

    add version.rb

---
+++ lib/version.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+# coding=utf-8
+
+# DO NOT TOUCH, AUTOMATICALLY UPDATED!
+module PianoServer
+  VERSION = '0.1.0'
+  BUILDID = '970b0b573e8979112bce68f154d8907045b69f26'
+  DEV_CODE = '0.1.0'
+end
