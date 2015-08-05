b88744061c859242f47c39cf900334ae48743999
Hysios Hu <hysios@gmail.com>
2015-08-05 13:00:38 +0800

    user correct config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,10 @@
-host = ENV['REDIS_PORT_6379_TCP_ADDR'] || 'redis://localhost:6379'
+host = ENV['REDIS_PORT_6379_TCP_ADDR'] || 'localhost'
+port = ENV['REDIS_PORT_6379_TCP_PORT'] || 6379
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: host }
+  config.redis = { host: host, port: port }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: host }
+  config.redis = { host: host, port: port }
 end
