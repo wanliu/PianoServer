5507bbd129f1c37a4414e33f3749414fbab8d493
zhangrui <517144093@qq.com>
2015-08-05 14:21:08 +0800

    增加拒绝按钮点击功能

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -74,7 +74,6 @@ class OrdersController < ApplicationController
     update_hash = @order.update_hash
     if @order.accept_state == 'accepting'
       update_hash["accept_state"] = "accept"
-      pp update_hash
       @order.update(accept_state: "accept")
       @order.update_patch(update_hash)
       # @order.items_attributes = update_hash["items"]
@@ -101,7 +100,8 @@ class OrdersController < ApplicationController
     @order.update(:accept_state => "reject")
     @order.update(updates: nil)
     MessageSystemService.push_command current_anonymous_or_user.id, other_side, {command: 'order', accept: 'reject'}.to_json
-    render json: { accept_state: @order.accept_state }
+
+    render :show, formats: [:json]
   end
 
   private
