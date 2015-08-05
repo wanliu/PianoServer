e9b320eaf8f6fa2d544f3dd28f7276f7101e59c2
Hysios Hu <hysios@gmail.com>
2015-08-05 13:06:46 +0800

    use url to config redis

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,10 +1,11 @@
 host = ENV['REDIS_PORT_6379_TCP_ADDR'] || 'localhost'
 port = ENV['REDIS_PORT_6379_TCP_PORT'] || 6379
+url = ENV['REDIS_PORT_6379_TCP'] || 'redis://localhost:6379'
 
 Sidekiq.configure_server do |config|
-  config.redis = { host: host, port: port }
+  config.redis = { url: url}
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { host: host, port: port }
+  config.redis = { url: url }
 end
