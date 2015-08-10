59a006b329aad95a1e6fc1c4562c1c27749132e3
liulei <satbirdd@gmail.com>
2015-08-10 13:37:53 +0800

    all item without paginate

--- app/controllers/shop_categories_controller.rb
+++ app/controllers/shop_categories_controller.rb
@@ -4,8 +4,8 @@ class ShopCategoriesController < ApplicationController
   end
 
   def show
-    page = params[:page].presence || 1
-    per = params[:per].presence || 9
+    # page = params[:page].presence || 1
+    # per = params[:per].presence || 9
 
     @shop_category = ShopCategory.find(params[:id])
     @shop = Shop.find(@shop_category.shop_id)
@@ -14,7 +14,7 @@ class ShopCategoriesController < ApplicationController
       @shop_categories = ShopCategory.where(parent_id: @shop_category)
       @items = []
     else
-      @items = Item.where(shop_category_id: params[:id], page: page, per: per)
+      @items = Item.where(shop_category_id: params[:id])#, page: page, per: per)
       @shop_categories = []
     end
   end
