3d2e9890cb70a425ef5538a29653c2dd075a2b4d
zhangrui <517144093@qq.com>
2015-08-17 10:37:18 +0800

    增加活动开始或结束的倒计时

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -102,7 +102,7 @@ class OrdersController < ApplicationController
   def reject
     @order.update(:accept_state => "reject")
     @order.update(updates: nil)
-    msg = '拒绝了您提交的修改'
+    msg = '拒绝了提交的修改'
 
     MessageSystemService.push_command current_anonymous_or_user.id, other_side, {command: 'order', accept: 'reject'}.to_json
 
