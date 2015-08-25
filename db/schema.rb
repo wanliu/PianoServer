72e665d1519263c4bdcc305b5afffe4dd45e6c82
Hysios Hu <hysios@gmail.com>
2015-08-25 13:01:53 +0800

    add type to Template

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150824072730) do
+ActiveRecord::Schema.define(version: 20150825050032) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -224,9 +224,8 @@ ActiveRecord::Schema.define(version: 20150824072730) do
     t.datetime "start_at"
     t.datetime "end_at"
     t.string   "condition"
-    t.text     "templates",   default: [],              array: true
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "templates", force: :cascade do |t|
@@ -236,6 +235,7 @@ ActiveRecord::Schema.define(version: 20150824072730) do
     t.integer  "subject_id"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.string   "type"
   end
 
   create_table "users", force: :cascade do |t|
