c3b169deeeff4ff5d88b69d8895481a27b7b2121
zhangrui <517144093@qq.com>
2015-08-31 16:08:24 +0800

    修改地址添加逻辑

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -16,9 +16,9 @@ class LocationsController < ApplicationController
 
     if @location.save
       # render json: @location, status: :created
-      @order = Order.find(@location.order_id)
-      @order.delivery_location_id = @location.id
-      @order.save
+      #@order = Order.find(@location.order_id)
+      # @order.delivery_location_id = @location.id
+      #@order.save
 
       redirect_to @location.chat_id ? chat_path(@location.chat_id) : location_path(@location)
     else
