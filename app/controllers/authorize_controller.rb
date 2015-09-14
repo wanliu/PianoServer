9b784dfbe1f02a80a659c9e88e7b36505de52d07
Billy.Zheng <zw963@163.com>
2015-09-14 18:16:18 +0800

    Add weixin authenticate basically function.

---
+++ app/controllers/authorize_controller.rb
@@ -0,0 +1,42 @@
+class AuthorizeController < ApplicationController
+  def weixin
+    redirect_url = "#{Settings.app.host}/weixin_redirect_url"
+    redirect_to $client.authorize_url(redirect_url, 'snsapi_userinfo') if $client.is_valid?
+  end
+
+  def weixin_redirect_url
+    code = params[:code]
+
+    if $client.is_valid?
+      access_token = $client.get_oauth_access_token(code).result['access_token']
+      # profile 返回一个哈希, 格式如下:
+      # {
+      #   "openid"=>"o0tInv8I0VvpdNyx12tMTy9asT-0",
+      #   "nickname"=>"羁绊",
+      #   "sex"=>1,
+      #   "language"=>"zh_CN",
+      #   "city"=>"太原",
+      #   "province"=>"山西",
+      #   "country"=>"中国",
+      #   "headimgurl"=>"http://wx.qlogo.cn/mmopen/Ccnk8Z7hLeMQsChtJJugQ6ronzJsp31TeickDmOBAl2ibRnsgxP3KZpx9LSAGDefATebLVU1Op6wWNyFVXr9H9LBhic1Egws9zE/0",
+      #   "privilege"=>[]
+      # }
+      profile = $client.get_oauth_userinfo('wx41b2029d52fb7900', access_token).result
+
+      unless User.where("data @> ?", {weixin_openid: profile['openid']}.to_json).empty?
+        User.create(
+          weixin_openid: profile['openid'],
+          username: profile['nickname'],
+          sex: profile['sex'],
+          image: profile['headimgurl'],
+          languange: profile['language'],
+          city: profile['city'],
+          province: profile['province'],
+          country: profile['country'],
+          weixin_privilege: profile['privilege']
+        )
+      end
+      redirect_to :back
+    end
+  end
+end
