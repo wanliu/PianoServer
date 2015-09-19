e7faa586ec4838c4fccdbbceb688596c8a5131c3
Hysios Hu <hysios@gmail.com>
2015-09-19 18:25:04 +0800

    refactory shop navbar title

--- app/controllers/shops/admin/base_controller.rb
+++ app/controllers/shops/admin/base_controller.rb
@@ -2,9 +2,8 @@ class Shops::Admin::BaseController < ApplicationController
   layout "shop_admin"
 
   before_action :set_shop
-  before_action :shop_page_title
+  before_action :shop_page_info
   before_action :authenticate_shop_user!
-
 
   def set_shop
     @shop = Shop.find_by name: params[:shop_id]
@@ -14,10 +13,10 @@ class Shops::Admin::BaseController < ApplicationController
     "shop"
   end
 
-  def shop_page_title
+  def shop_page_info
     self.page_title += [ t("titles.shops", shop_name: @shop.title) ]
+    self.page_navbar = @shop.title
   end
-
 
   def authenticate_shop_user!
     user_id = current_user.try(:id)
--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,9 +1,11 @@
 class ShopsController < ApplicationController
   before_action :prepare_shop_views_path
+  before_action :set_shop, only: [ :show_by_name, :show ]
+  before_action :shop_page_info
 
   def show_by_name
-    @shop = Shop.find_by name: params[:shop_name]
-
+    set_page_title @shop.title
+    # @title = @shop.title
     page = params[:page].presence || 1
     per = params[:per].presence || 9
 
@@ -30,5 +32,15 @@ class ShopsController < ApplicationController
 
   def prepare_shop_views_path
     prepend_view_path Settings.sites.shops.root
+  end
+
+  def set_shop
+    @shop = Shop.find_by name: params[:shop_name]
+  end
+
+  def shop_page_info
+    self.page_title += [ t("titles.shops", shop_name: @shop.title) ]
+    self.page_navbar = @shop.title
+    self.page_navbar_link = shop_site_path(@shop.name)
   end
 end
