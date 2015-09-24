cd7e2ab698f19309633fae7468a2a74573a5de51
liulei <satbirdd@gmail.com>
2015-09-24 16:48:31 +0800

    validates

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -2,5 +2,8 @@ class Unit < ActiveRecord::Base
 
   has_many :properties
 
+  validates :name, presence: true, uniqueness: true
+  validates :title, presence: true, uniqueness: true
+
   paginates_per 10
 end
