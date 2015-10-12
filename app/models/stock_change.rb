5af1c7a0836244b57c7c0c336485a49c3f032cc9
liulei <satbirdd@gmail.com>
2015-10-12 14:54:37 +0800

    relations of stock changes

--- app/models/stock_change.rb
+++ app/models/stock_change.rb
@@ -1,6 +1,6 @@
 class StockChange < ActiveRecord::Base
   belongs_to :item
   belongs_to :unit
-  belongs_to :operator
-  belongs_to :operation
+  belongs_to :operator, class_name: 'User'
+  belongs_to :operation, polymorphic: true
 end
