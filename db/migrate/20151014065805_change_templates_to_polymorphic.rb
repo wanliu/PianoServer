c10633f06c38947758b05e929b3c87cc47005b79
Hysios Hu <hysios@gmail.com>
2015-10-14 15:42:24 +0800

    add overcommit

--- db/migrate/20151014065805_change_templates_to_polymorphic.rb
+++ db/migrate/20151014065805_change_templates_to_polymorphic.rb
@@ -2,15 +2,16 @@ class ChangeTemplatesToPolymorphic < ActiveRecord::Migration
   def up
     change_table :templates do |t|
       t.references :templable, :polymorphic => true
-      t.remove_column :subject_id
     end
+
+    remove_column :templates, :subject_id
   end
 
   def down
     change_table :templates do |t|
       t.remove_references :templable, :polymorphic => true
-      t.add_column :subject_id, :integer
     end
+
+    add_column templates:, :subject_id, :integer
   end
-end
 end
