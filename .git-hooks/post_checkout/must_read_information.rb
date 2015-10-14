abf045041c5feb8726c5e9814dbe685e9c7dee5b
Hysios Hu <hysios@gmail.com>
2015-10-14 17:35:12 +0800

    add ignore read files

--- .git-hooks/post_checkout/must_read_information.rb
+++ .git-hooks/post_checkout/must_read_information.rb
@@ -4,9 +4,14 @@ module Overcommit::Hook::PostCheckout
 
     def run
       result = execute(['sh', SCRIPT_LOCATION])
+      puts last_read_commit
       return :pass if result.success?
 
       [:fail, result.stdout]
+    end
+
+    def last_read_commit
+      Dir.glob(File.expand_path('../must_read_information/.read/*.*', __FILE__)).max { |a,b| File.ctime(a) <=> File.ctime(b) }
     end
   end
 end
