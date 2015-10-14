0882493e161b0ece615c17e19747db15f218e096
Hysios Hu <hysios@gmail.com>
2015-10-14 17:44:40 +0800

    must_read:   这里是技术性问题测试。。。。
      ```ruby
        a = [1,2,3]
        puts a.sum
        # => 6
    
      ```

--- .git-hooks/post_checkout/must_read_information.rb
+++ .git-hooks/post_checkout/must_read_information.rb
@@ -3,15 +3,16 @@ module Overcommit::Hook::PostCheckout
     SCRIPT_LOCATION = File.expand_path('../scripts/git_important.sh', __FILE__)
 
     def run
-      result = execute(['sh', SCRIPT_LOCATION])
       puts last_read_commit
+
+      result = execute(['sh', SCRIPT_LOCATION, last_read_commit])
       return :pass if result.success?
 
       [:fail, result.stdout]
     end
 
     def last_read_commit
-      Dir.glob(File.expand_path('../must_read_information/.read/*.*', __FILE__)).max { |a,b| File.ctime(a) <=> File.ctime(b) }
+      File.basename(Dir.glob(File.expand_path('../must_read_information/.read/*', __FILE__)).max { |a,b| File.ctime(a) <=> File.ctime(b) })
     end
   end
 end
