2adad70f66ca04003b95be2f9c066c0254448c14
liulei <satbirdd@gmail.com>
2015-11-03 17:07:15 +0800

    code format

--- lib/content_management/controller_helper.rb
+++ lib/content_management/controller_helper.rb
@@ -27,8 +27,15 @@ module ContentManagement
         end
 
         if with_object.respond_to? :templates
-          variables = with_object.templates.includes(:variables).inject([]) {|s, t| s.concat(t.variables) }.uniq { |var| var.name }
-          attachments = with_object.templates.includes(:attachments).inject([]) {|s, t| s.concat(t.attachments) }.uniq { |var| var.name }
+          variables = with_object.templates.includes(:variables)
+            .inject([]) {|s, t|
+              s.concat(t.variables)
+            }.uniq { |var| var.name }
+
+          attachments = with_object.templates.includes(:attachments)
+            .inject([]) {|s, t|
+              s.concat(t.attachments)
+            }.uniq { |var| var.name }
 
           load_all_variables variables
           load_attachments attachments
