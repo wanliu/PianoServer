e86aae99c2de9dfee0585592bf9b4e5fc4f3212a
zhangrui <517144093@qq.com>
2015-11-06 16:50:27 +0800

    修改微信登录显示与点击商店名跳转

--- app/drops/promotion_drop.rb
+++ app/drops/promotion_drop.rb
@@ -2,4 +2,8 @@ class PromotionDrop < Liquid::Rails::Drop
   attributes :id, :type, :title, :image_url, :product_price, :product_inventory, :publisher_avatar,
     :shop_id, :category_id, :status, :discount_price, :discount, :avatar,
     :shop_name, :created_at, :updated_at
+
+  def shop_name
+    object.shop.try(:name)
+  end
 end
