26d3909efb42145b4a63b22a284cc182cd39cc5e
yinz <yinz0819@vip.qq.com>
2015-11-09 16:52:38 +0800

    修复subject模板

--- app/drops/promotion_drop.rb
+++ app/drops/promotion_drop.rb
@@ -1,5 +1,13 @@
 class PromotionDrop < Liquid::Rails::Drop
   attributes :id, :type, :title, :image_url, :product_price, :product_inventory, :publisher_avatar,
     :shop_id, :category_id, :status, :discount_price, :discount, :avatar,
-    :shop_name, :created_at, :updated_at
+    :shop_name, :created_at, :updated_at, :shop_title
+
+  def shop_name
+    object.shop.try(:name)
+  end
+
+  def shop_title
+    object.shop.try(:title)
+  end
 end
