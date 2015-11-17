f0c0493eeeb5b02cdd7c423c527d6cf99d9b1f11
yinz <yinz0819@vip.qq.com>
2015-11-17 18:05:40 +0800

    pc 找回密码 重置密码成功了 mobile待做

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,8 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: {
     sessions: 'users/sessions',
-    registrations: 'users/registrations'
+    registrations: 'users/registrations',
+    passwords: 'users/passwords'
   }
 
   mount ChinaCity::Engine => '/china_city'
