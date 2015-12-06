c7d291c1573de5fad33ba5ae70ecb36e9fae5be7
liulei <satbirdd@gmail.com>
2015-12-06 17:58:08 +0800

    地址管理

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,9 +1,17 @@
 class LocationsController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create, :udpate, :destroy, :user_default_address]
+  before_action :set_location, only: [:edit, :update, :destroy]
 
+  def index
+    @locations = current_user.locations.order(id: :desc)
+  end
 
   def new
     @location = Location.new(user_id: current_anonymous_or_user.id,
       chat_id: params[:chat_id], intention_id: params[:intention_id])
+  end
+
+  def edit
   end
 
   def show
@@ -13,7 +21,7 @@ class LocationsController < ApplicationController
 
   def create
     # location = current_anonymous_or_user.locations.build(location_params)
-    @location = Location.new(location_params)
+    @location = current_user.locations.build(location_params)
 
     respond_to do |format|
       if @location.save
@@ -22,7 +30,11 @@ class LocationsController < ApplicationController
         # @intention.delivery_location_id = @location.id
         #@intention.save
         format.json { render :show }
-        format.html { redirect_to @location.chat_id ? chat_path(@location.chat_id) : location_path(@location) }
+        format.html do
+          flash[:notice] = "收货地址创建成功"
+          direction = @location.chat_id.present? ? chat_path(@location.chat_id) : locations_path
+          redirect_to direction
+        end
       else
         format.json { render :new, formats: :html, layout: false, status: :unprocessable_entity }
         format.html { render :new }
@@ -30,22 +42,51 @@ class LocationsController < ApplicationController
     end
   end
 
+  def update
+    respond_to do |format|
+      if @location.update(location_update_params)
+        format.json { render json: @location, status: :ok }
+        format.html do
+          flash[:notice] = "收货地址保存成功"
+          redirect_to locations_path
+        end
+      else
+        format.json { render json: {error: @location.errors} }
+        format.html { render :edit }
+      end
+    end
+  end
+
   def destroy
-    location = Location.find params[:id]
+    @location.destroy
+
+    flash[:notice] = "收货地址删除成功"
+    redirect_to locations_path
   end
 
   def user_default_address
-    @user = User.find params[:user_id];
-    @user.latest_location_id = params[:location_id];
-    @user.save
+    @location = current_user.locations.find(params[:location_id])
+    current_user.update_attribute('latest_location_id', @location.id)
 
-    render json: @user.latest_location
+    respond_to do |format|
+      format.json { render json: @location }
+      format.html { redirect_to locations_path }
+    end
   end
 
   private
 
   def location_params
     params.require(:location)
-      .permit(:province_id,:city_id,:region_id,:contact, :id, :road, :zipcode, :contact_phone, :user_id, :chat_id, :intention_id)
+      .permit(:province_id, :city_id, :region_id, :contact, :id, :road, :zipcode, :contact_phone, :chat_id, :intention_id)
+  end
+
+  def location_update_params
+    params.require(:location)
+      .permit(:province_id, :city_id, :region_id, :contact, :id, :road, :zipcode, :contact_phone, :is_default)
+  end
+
+  def set_location
+    @location = current_user.locations.find(params[:id])
   end
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -9,13 +9,22 @@ class Location < ActiveRecord::Base
   VALID_PHONE_REGEX = /\A((\d{3,4}-\d{7,8}(-\d+)?)|((\+?86)?1\d{10}))\z/
   VALID_CONTACT_REGEX = /([\u4e00-\u9fa5]|[a-zA-Z0-9_]|[\uFF10-\uFF19])+/
 
+  validates :contact, presence: true,
+    length: { maximum: 30 },
+    format: { with: VALID_CONTACT_REGEX, :allow_blank => true },
+    unless: :skip_validation
 
-  validates :contact, presence: true, length: { maximum: 30 }, format: { with: VALID_CONTACT_REGEX, :allow_blank => true }, on: :create, unless: :skip_validation
-  validates :road, presence: true, length: { maximum: 140 }, on: :create, unless: :skip_validation
-  validates :contact_phone, presence: true, format: { with: VALID_PHONE_REGEX, :allow_blank => true }, on: :create, unless: :skip_validation
-  validates :province_id, presence: true, on: :create
-  validates :city_id, presence: true, on: :create
-  validates :region_id, presence: true, on: :create
+  validates :contact_phone, presence: true,
+    format: { with: VALID_PHONE_REGEX, :allow_blank => true },
+    unless: :skip_validation
+
+  validates :road, presence: true,
+    length: { maximum: 140 },
+    unless: :skip_validation
+
+  validates :province_id, presence: true
+  validates :city_id, presence: true
+  validates :region_id, presence: true
 
   validate :too_many_record, unless: :skip_validation
 
@@ -54,5 +63,9 @@ class Location < ActiveRecord::Base
     if locations.length >= AMOUNT_LIMIT
       errors.add(:amount, '已达到上限')
     end
+  end
+
+  def is_default?
+    id == user.latest_location_id
   end
 end
