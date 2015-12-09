0a3a829265967baf152fe12ab7bf42bc1e5a0f1c
liulei <satbirdd@gmail.com>
2015-12-09 15:13:20 +0800

    use instant to store proc

--- app/tables/user_table.rb
+++ app/tables/user_table.rb
@@ -2,6 +2,34 @@ class UserTable < TableCloth::Base
   # Define columns with the #column method
   column :id, :username, :email, :mobile, :nickname, :current_sign_in_ip, :sign_in_count, :created_at, :updated_at
 
+  UPLOAD_HELPER = Proc.new do |user|
+    <<-HTML
+      <img src='#{user.image.url(:cover)}' height='50' width='50' class='upload-image' data-user-id='#{user.id}'>
+      <div type="button" class="uploader-btn" data-user-id='#{user.id}'>upload</div>
+      <script type="text/javascript">
+        (function() {
+          var token = $("meta[name=csrf-token]").attr('content');
+          var action = "/admins/accounts/upload_user_avatar?user_id=#{user.id}";
+          var $button = $(".uploader-btn[data-user-id=#{user.id}]");
+          var $img = $("img[data-user-id=#{user.id}]");
+
+          function uploadSucc(data, status, xhr) {
+            var url = xhr.url;
+            $img.attr('src', url);
+          }
+          
+          $uploader = new qq.FileUploader({
+            element: $button[0],
+            action: action,
+            customHeaders: { "X-CSRF-Token": token },
+            multiple: false,
+            onComplete: uploadSucc
+          });
+        })();
+      </script>
+    HTML
+  end
+
   # Columns can be provided a block
   #
   # column :name do |object|
@@ -25,35 +53,7 @@ class UserTable < TableCloth::Base
   # Make sure you include the actions extension.
   #
   actions do
-    html = Proc.new do |user|
-      <<-HTML
-        <img src='#{user.image}' height='50' width='50' class='upload-image' data-user-id='#{user.id}'>
-        <div type="button" class="uploader-btn" data-user-id='#{user.id}'>upload</div>
-        <script type="text/javascript">
-          (function() {
-            var token = $("meta[name=csrf-token]").attr('content');
-            var action = "/admins/accounts/upload_user_avatar?user_id=#{user.id}";
-            var $button = $(".uploader-btn[data-user-id=#{user.id}]");
-            var $img = $("img[data-user-id=#{user.id}]");
-
-            function uploadSucc(data, status, xhr) {
-              var url = xhr.url;
-              $img.attr('src', url);
-            }
-            
-            $uploader = new qq.FileUploader({
-              element: $button[0],
-              action: action,
-              customHeaders: { "X-CSRF-Token": token },
-              multiple: false,
-              onComplete: uploadSucc
-            });
-          })();
-        </script>
-      HTML
-    end
-
-    action { |user| html.call(user).html_safe }
+    action { |user| UPLOAD_HELPER.call(user).html_safe }
   end
   #
   # If action provides an "if:" option, it will call that method on the object. It can also take a block with an arity of 1.
