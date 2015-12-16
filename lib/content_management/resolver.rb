2962d3d3eb4f1c84bc8ae01fe8689e0308de71bc
Hysios Hu <hysios@gmail.com>
2015-12-16 10:25:13 +0800

    fixed template_object bug

--- lib/content_management/resolver.rb
+++ lib/content_management/resolver.rb
@@ -9,17 +9,19 @@ module ContentManagement
 
   private
 
+    # 利用 ActionView::Resolver 自身的缓存机制来进行缓存，在 options 压入 template_object
+    # 后，在 locals 中生成一个临时变量使缓存工作
     def cached_with_template(key, path_info, details, locals) #:nodoc:
       name, prefix, partial = path_info
       locals = locals.map { |x| x.to_s }.sort!
 
-      if details[:template_object]
+      unless details[:template_object].blank?
         locals.push(details[:template_object])
       end
 
       if key
         @cache.cache(key, name, prefix, partial, locals) do
-          locals.pop if details[:template_object]
+          locals.pop unless details[:template_object].blank?
           decorate(yield, path_info, details, locals)
         end
       else
