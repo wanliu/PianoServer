9ba914e74eb1db0ca9e96f6ffdd04e3fed69f4a2
Hysios Hu <hysios@gmail.com>
2015-12-30 17:58:59 +0800

    paramlize status query and time diff query

--- app/controllers/api/promotions/one_money_controller.rb
+++ app/controllers/api/promotions/one_money_controller.rb
@@ -38,6 +38,26 @@ class Api::Promotions::OneMoneyController < Api::BaseController
     hash[:signup_count] = @one_money.signups.count
     hash[:participant_count] =  @one_money.participants.count
     hash[:winner_count] =  @one_money.winners.count
+
+    if params[:u].present?
+      hash[:td] = (Time.now.to_f * 1000) - params[:u].to_i
+    end
+
+    if params[:signups].present?
+      s = [params[:signups].to_i, 50].min
+      hash[:signups] = @one_money.signups.sort(by: :created_at, order: 'desc', limit: [0, s]).map {|u| u.attributes }
+    end
+
+    if params[:participants].present?
+      s = [params[:participants].to_i, 50].min
+      hash[:participants] = @one_money.participants.sort(by: :created_at, order: 'desc', limit: [0, s]).map {|u| u.attributes }
+    end
+
+    if params[:winners].present?
+      s = [params[:winners].to_i, 50].min
+      hash[:winners] = @one_money.winners.sort(by: :created_at, order: 'desc', limit: [0, s]).map {|u| u.attributes }
+    end
+
     render json: hash
   end
 
