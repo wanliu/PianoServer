72c47f84e8c6eb4019d4ac2483f3dc5ad5f1e4bf
yinz <yinz0819@vip.qq.com>
2016-01-01 18:00:42 +0800

    手机上只有微信里 $.ajax有效果！什么问题？

--- public/one_money/src/script/index.js
+++ public/one_money/src/script/index.js
@@ -1,17 +1,16 @@
 $(function() {
   $.ajax({
     url:'/api/promotions/one_money/1/items',
-    data: {},
+    type:'GET',
     success: function(itemsData) {
-      console.log(itemsData);
+      alert(itemsData);
       itemsData.map(function(itemData) {
-        console.log(itemData);
         var imgUrl = JSON.parse(itemData.cover_urls)[0];
-        var template = '<li>
-                          <a href="list.html#'+ itemData.id +'">
-                            <img src="'+ imgUrl+' "/>
-                            <div class="price-wrap">￥'+ itemData.price +'</div>
-                          </a>
+        var template = '<li>\
+                          <a href="list.html#'+ itemData.id +'">\
+                            <img src="'+ imgUrl+' "/>\
+                            <div class="price-wrap">￥'+ itemData.price +'</div>\
+                          </a>\
                         </li>'
         $('.preview-list').append(template)
       });
--- public/one_money/src/script/list.js
+++ public/one_money/src/script/list.js
@@ -1,7 +1,6 @@
 $(function() {
   $.ajax({
     url:'/api/promotions/one_money/1/items',
-    data: {},
     success: function(itemsData) {
       console.log(itemsData);
       itemsData.map(function(itemData) {
@@ -59,24 +58,24 @@ PromotionItem.prototype = {
     } 
   },
   template: function() {
-    return '<li class="promotion-item" name='+ this.id +'>
+    return '<li class="promotion-item" name='+ this.id +'>\
               <a class="item-wrap" href="detail.html?one_money_id='+ this.one_money_id+'&id='+ this.id +'">\
-                <header>
-                  '+ this.statusFlagTemplate() +'
-                  <img src="'+ this.cover_urls[0] +'">
-                  <div class="limit">
-                    <span class="count-time"></span>
-                  </div>
-                </header>
-                <main>
-                  <p class="title">'+ this.title +'</p>
-                  <hr>
-                  <div class="price-wrap">
-                    <span class="now-price">￥'+ this.price +'</span>
-                    &emsp;<s class="origin-price">￥'+ this.ori_price +'</s>
-                  </div>
-                </main>
-              </a>
+                <header>\
+                  '+ this.statusFlagTemplate() +'\
+                  <img src="'+ this.cover_urls[0] +'">\
+                  <div class="limit">\
+                    <span class="count-time"></span>\
+                  </div>\
+                </header>\
+                <main>\
+                  <p class="title">'+ this.title +'</p>\
+                  <hr>\
+                  <div class="price-wrap">\
+                    <span class="now-price">￥'+ this.price +'</span>\
+                    &emsp;<s class="origin-price">￥'+ this.ori_price +'</s>\
+                  </div>\
+                </main>\
+              </a>\
             </li>';
   },
   render: function(target) {
