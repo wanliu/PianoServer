d049b056f883f7596dd7eca4ea79e8b7eb1975b9
yinz <yinz0819@vip.qq.com>
2016-01-05 14:22:18 +0800

    定时改变状态

--- public/one_money/src/script/list.js
+++ public/one_money/src/script/list.js
@@ -32,19 +32,41 @@ function PromotionItem(itemData) {
 
 PromotionItem.prototype = {
   getStatus: function() {
-    if (this.status != "timing")     return this.status;
-    if (this.total_amount < 1)       return 'shortage';
-    if (Date.now() < this.start_at ) return 'wait';
-    if (Date.now() > this.end_at)    return 'expired';
-    
-    return 'started';
+    var now = new Date().getTime();
+    if (this.status != "timing") return this.status;
+    if (this.total_amount < 1)   return 'shortage';
+    if (now < this.start_at )    return 'wait';
+    if (now > this.end_at)       return 'expired';
+    return 'wait';
+  },
+  changeStatus: function(status) {
+    $('.promotion-item[name='+this.id+'] .status-wrap').html(this.statusFlagTemplate(status));
   },
   countDownTime: function() {
-    var element = $('.promotion-item[name='+ this.id +'] .count-time');
-    new CountDown(element, +this.total_amount, this.start_at, this.end_at);
+    var status = this.getStatus();
+    if (status == 'end' || status == 'shortage') return;
+
+    var _this = this;
+    var timeManager = CountDownManager.getManager();
+    timeManager.addHandler(handler); // 添加定时器
+
+    function handler() {
+      var now = new Date().getTime();
+
+      if (now > _this.end_at && status != 'end'){
+        status = 'end';
+        timeManager.removeHandler(handler); // 移除定时器
+        return _this.changeStatus('end');
+      }
+      if (now > _this.start_at && status != 'started') {
+        status = 'started';
+        return _this.changeStatus('started');
+      }
+    }
   },
-  statusFlagTemplate: function() {
-    switch (this.getStatus()) {
+  statusFlagTemplate: function(status) {
+    var status = status || this.getStatus();
+    switch (status) {
       case 'wait':
         return '<span class="status wait">未开始</span>&emsp;参与人数:' + this.total_amount;
 
@@ -59,25 +81,25 @@ PromotionItem.prototype = {
     } 
   },
   template: function() {
-    return '<li class="promotion-item" name='+ this.id +'>\
-              <a class="item-wrap" href="detail.html?one_money_id='+ this.one_money_id+'&id='+ this.id +'">\
-                <header>\
-                  <img class="one-money-logo" src="images/one_money_log.jpg"/>\
-                  <img class="cover" src="'+ this.cover_urls[0] +'">\
-                  <div class="limit">\
-                    <span class="count-time"></span>\
-                  </div>\
-                </header>\
-                <main>\
-                  <p class="title">'+ this.title +'</p>\
-                  <div class="price-wrap">\
-                    <span class="now-price">￥'+ this.price +'</span>\
-                    <span class="origin-price">原价<s>￥'+ this.ori_price +'&nbsp;</s></span>\
-                  </div>\
-                  <div class="status-wrap">'+ this.statusFlagTemplate() +'</div>\
-                </main>\
-              </a>\
-            </li>';
+    return '\
+      <li class="promotion-item" name='+ this.id +'>\
+        <a class="item-wrap" href="detail.html?one_money_id='+ this.one_money_id+'&id='+ this.id +'">\
+          <header>\
+            <img class="one-money-logo" src="images/one_money_log.jpg"/>\
+            <img class="cover" src="'+ this.cover_urls[0] +'">\
+            <div class="limit">\
+            </div>\
+          </header>\
+          <main>\
+            <p class="title">'+ this.title +'</p>\
+            <div class="price-wrap">\
+              <span class="now-price">￥'+ this.price +'</span>\
+              <span class="origin-price">原价<s>￥'+ this.ori_price +'&nbsp;</s></span>\
+            </div>\
+            <div class="status-wrap">'+ this.statusFlagTemplate() +'</div>\
+          </main>\
+        </a>\
+      </li>';
   },
   render: function(target) {
     var target = target || $('.promotions-list');
