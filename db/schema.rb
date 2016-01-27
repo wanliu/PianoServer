5553a56232fe31ea8e7d7929337951730b7fc71b
yinz <yinz0819@vip.qq.com>
2016-01-27 13:51:02 +0800

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20160127051649) do
 
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "hstore"
