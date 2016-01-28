f64dcd1a87e8c4d162cfa8e116beac58fb248cc9
yinz <yinz0819@vip.qq.com>
2016-01-28 18:31:22 +0800

    增加order_id

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -136,8 +136,8 @@ class OrdersController < ApplicationController
         format.html do
           # 一元购收货后跳到评价页面
           if @order.wait_for_yiyuan_evaluate?
-            one_money = OneMoney[@order.one_money]
-            redirect_to "/one_money/#{ one_money.start_at.strftime('%Y-%m-%d') }/index.html#/comment/#{ @order.pmo_grab_id }"
+            one_money = OneMoney[@order.one_money_id]
+            redirect_to "/one_money/#{ one_money.start_at.strftime('%Y-%m-%d') }/index.html#/comment/#{ @order.pmo_grab_id }/#{@order.id}"
           else
             redirect_to history_orders_path
           end
