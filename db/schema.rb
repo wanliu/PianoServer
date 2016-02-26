aa22e415d4ed99143ce341bb819460ac3c239e83
Hysios Hu <hysios@gmail.com>
2016-02-26 14:47:50 +0800

    fixed fare to_has incorrect bug

--- db/schema.rb
+++ db/schema.rb
@@ -261,6 +261,17 @@ ActiveRecord::Schema.define(version: 20160222023330) do
     t.datetime "updated_at",                null: false
   end
 
+  create_table "likes", force: :cascade do |t|
+    t.string   "liker_type"
+    t.integer  "liker_id"
+    t.string   "likeable_type"
+    t.integer  "likeable_id"
+    t.datetime "created_at"
+  end
+
+  add_index "likes", ["likeable_id", "likeable_type"], name: "fk_likeables", using: :btree
+  add_index "likes", ["liker_id", "liker_type"], name: "fk_likes", using: :btree
+
   create_table "line_items", force: :cascade do |t|
     t.integer  "itemable_id"
     t.string   "itemable_type"
@@ -300,6 +311,17 @@ ActiveRecord::Schema.define(version: 20160222023330) do
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
   end
+
+  create_table "mentions", force: :cascade do |t|
+    t.string   "mentioner_type"
+    t.integer  "mentioner_id"
+    t.string   "mentionable_type"
+    t.integer  "mentionable_id"
+    t.datetime "created_at"
+  end
+
+  add_index "mentions", ["mentionable_id", "mentionable_type"], name: "fk_mentionables", using: :btree
+  add_index "mentions", ["mentioner_id", "mentioner_type"], name: "fk_mentions", using: :btree
 
   create_table "messages", force: :cascade do |t|
     t.integer  "messable_id"
@@ -454,8 +476,8 @@ ActiveRecord::Schema.define(version: 20160222023330) do
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
     t.string   "logo"
-    t.jsonb    "settings",    default: {}
     t.string   "address"
+    t.jsonb    "settings",    default: {}
     t.integer  "shop_type",   default: 0
     t.float    "lat"
     t.float    "lon"
