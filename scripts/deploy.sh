426d4277078e0f6b536bab984f4f1954b94f5296
Hysios Hu <hysios@gmail.com>
2016-03-04 18:02:51 +0800

    add variable in script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
+
+export SECRET_KEY_BASE="XXX"
+export LIVE_KEY_BASE="XXX"
+export INSTANCE_ID=`wget -q -O - http://instance-data/latest/meta-data/instance-id`
+
+export DATABASE_HOST=`aws ec2 describe-tags --filters "Name=key,Values=PostgresHost" | awk '\$3 = $INSTANCE_ID { print \$5 }'`
+export DATABASE_PORT=5432
+export RAILS_ENV=production
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-$DIR/var.sh
 
 echo "Database Host: $DATABASE_HOST"
 
--- scripts/init.sh
+++ scripts/init.sh
@@ -4,8 +4,13 @@ set -e
 # since nginx and unicorn accept the same signals
 
 # Feel free to change any of the following variables for your app:
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-$DIR/var.sh
+export SECRET_KEY_BASE="XXX"
+export LIVE_KEY_BASE="XXX"
+export INSTANCE_ID=`wget -q -O - http://instance-data/latest/meta-data/instance-id`
+
+export DATABASE_HOST=`aws ec2 describe-tags --filters "Name=key,Values=PostgresHost" | awk '\$3 = $INSTANCE_ID { print \$5 }'`
+export DATABASE_PORT=5432
+export RAILS_ENV=production
 
 TIMEOUT=${TIMEOUT-60}
 APP_ROOT=/var/www/PianoServer
