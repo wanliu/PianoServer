bd00e50222ba2f0b12f09ca2dc77fbf8f9327203
Hysios Hu <hysios@gmail.com>
2016-03-04 14:44:48 +0800

    update database config

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 ID=`wget -q -O - http://instance-data/latest/meta-data/instance-id`
-DATABASE_HOST=`aws ec2 describe-tags --filters "Name=key,Values=PostgresHost" | awk '\$3 = $ID  { print \$5 }'`
-DATABASE_PORT=5432
+export DATABASE_HOST=`aws ec2 describe-tags --filters "Name=key,Values=PostgresHost" | awk '\$3 = $ID  { print \$5 }'`
+export DATABASE_PORT=5432
 echo "Database Host: $DATABASE_HOST"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PREFIX=" bundle exec "
