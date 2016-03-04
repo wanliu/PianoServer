512aad0af5887f4bc4ffd222fb73176767530e15
Hysios Hu <hysios@gmail.com>
2016-03-04 14:48:13 +0800

    update database config

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,10 +3,10 @@
 ID=`wget -q -O - http://instance-data/latest/meta-data/instance-id`
 export DATABASE_HOST=`aws ec2 describe-tags --filters "Name=key,Values=PostgresHost" | awk '\$3 = $ID  { print \$5 }'`
 export DATABASE_PORT=5432
+export RAILS_ENV=production
 echo "Database Host: $DATABASE_HOST"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-PREFIX=" bundle exec "
-RAILS_ENV=production
+PREFIX="bundle exec"
 
 $PREFIX rake db:migrate && rake assets:clean
 $PREFIX rake assets:precompile
