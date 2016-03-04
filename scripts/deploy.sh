5e44b270561e320f41ff2935ec1e6f773d5b8298
Hysios Hu <hysios@gmail.com>
2016-03-04 11:29:45 +0800

    upgrade script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-id = ${wget -q -O - http://instance-data/latest/meta-data/instance-id}
-
-export DATABASE_HOST = ${aws ec2 describe-tags --filters "Name=key,Values=PostgresHost" | awk '\$3 = $id  { print \$5 }'}
-export DATABASE_PORT = 5432
+ID = "${wget -q -O - http://instance-data/latest/meta-data/instance-id}"
+DATABASE_HOST = ${aws ec2 describe-tags --filters "Name=key,Values=PostgresHost" | awk '\$3 = $ID  { print \$5 }'}
+DATABASE_PORT = 5432
 
 rake db:migrate && rake assets:clean
 rake assets:precompile
