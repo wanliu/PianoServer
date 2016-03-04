78605db0e8cd29fe375fc814f21454100858525e
Hysios Hu <hysios@gmail.com>
2016-03-04 14:32:02 +0800

    update database config

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,11 +6,9 @@ DATABASE_PORT=5432
 echo "Database Host: $DATABASE_HOST"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PREFIX=" bundle exec "
-ENV="RAILS_ENV=production"
-CMD_PREFIX="DATABASE_HOST=\"$DATABASE_HOST\" $ENV $PREFIX "
+RAILS_ENV=production
 
-$CMD_PREFIX rake db:create 
-$CMD_PREFIX rake db:migrate && rake assets:clean
+$PREFIX rake db:migrate && rake assets:clean
 $PREFIX rake assets:precompile
 
 $DIR/init.sh upgrade
