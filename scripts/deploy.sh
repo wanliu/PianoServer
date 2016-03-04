0e11bf8aefc2baae4cd591191508a378e85384cd
Hysios Hu <hysios@gmail.com>
2016-03-04 11:57:13 +0800

    update scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 ID=`wget -q -O - http://instance-data/latest/meta-data/instance-id`
-DATABASE_HOST=`aws ec2 describe-tags --filters "Name=key,Values=PostgresHost" | awk '\$3 = $ID  { print \$5 }'`
-DATABASE_PORT=5432
+export DATABASE_HOST=`aws ec2 describe-tags --filters "Name=key,Values=PostgresHost" | awk '\$3 = $ID  { print \$5 }'`
+export DATABASE_PORT=5432
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 RAILS_ENV=production
 bundle exec rake db:migrate && rake assets:clean
