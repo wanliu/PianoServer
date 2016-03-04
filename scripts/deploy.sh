721a462afa4b56afe0545573102c54c7d97a8b43
Hysios Hu <hysios@gmail.com>
2016-03-04 11:49:27 +0800

    upgrade script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,8 +3,9 @@
 ID=`wget -q -O - http://instance-data/latest/meta-data/instance-id`
 DATABASE_HOST=`aws ec2 describe-tags --filters "Name=key,Values=PostgresHost" | awk '\$3 = $ID  { print \$5 }'`
 DATABASE_PORT=5432
-
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+RAILS_ENV=production
 bundle exec rake db:migrate && rake assets:clean
 bundle exec rake assets:precompile
 
-./init.sh upgrade
+$DIR/init.sh upgrade
