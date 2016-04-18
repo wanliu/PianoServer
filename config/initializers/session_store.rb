c21c1ce77126ac5fee99474eddaf62813881b06e
zhangrui <517144093@qq.com>
2016-04-18 13:44:30 +0800

    移除共享cookie

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,20 +1,20 @@
 # Be sure to restart your server when you modify this file.
 
-# Rails.application.config.session_store :cookie_store, key: '_PianoServer_session', :expire_after => 259200, :domain => :all
+Rails.application.config.session_store :cookie_store, key: '_PianoServer_session', :expire_after => 259200, :domain => :all
 
-options = {
-  key: '_PianoServer_session',
-  redis: {
-    db: 2,
-    expire_after: 3.days,
-    key_prefix: 'piano:session:',
-    host: Rails.application.config.redis_config["host"], # Redis host name, default is localhost
-    port: Rails.application.config.redis_config["port"]   # Redis port, default is 6379
-  }
-}
+# options = {
+#   key: '_PianoServer_session',
+#   redis: {
+#     db: 2,
+#     expire_after: 3.days,
+#     key_prefix: 'piano:session:',
+#     host: Rails.application.config.redis_config["host"], # Redis host name, default is localhost
+#     port: Rails.application.config.redis_config["port"]   # Redis port, default is 6379
+#   }
+# }
 
-if Rails.env.production?
-  options[:domain] = ".wanliu.biz"
-end
+# if Rails.env.production?
+#   options[:domain] = ".wanliu.biz"
+# end
 
-Rails.application.config.session_store :redis_session_store, options
+# Rails.application.config.session_store :redis_session_store, options
