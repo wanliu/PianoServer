6e0c536c34f6885a2d26d8b1fa0810ddb8d1eb16
liulei <satbirdd@gmail.com>
2016-06-08 16:31:51 +0800

    检查一元购商品的库存及上架状况 js

--- app/controllers/admins/one_money_controller.rb
+++ app/controllers/admins/one_money_controller.rb
@@ -109,6 +109,7 @@ class Admins::OneMoneyController < Admins::BaseController
                                                  shop_name: item.shop.title,
                                                  sid: item.sid,
                                                  price: item.price,
+                                                 on_sale: item.on_sale,
                                                  inventory: item.current_stock }} }
   end
 
