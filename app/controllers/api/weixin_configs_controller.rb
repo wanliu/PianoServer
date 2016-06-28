60a428d126b18e1377014fb6b2ba2afceb49fba2
liulei <satbirdd@gmail.com>
2016-06-28 16:06:30 +0800

    ignores jsapilist

--- app/controllers/api/weixin_configs_controller.rb
+++ app/controllers/api/weixin_configs_controller.rb
@@ -9,7 +9,7 @@ class Api::WeixinConfigsController < Api::BaseController
       timestamp: signature[:timestamp], # 必填，生成签名的时间戳
       nonceStr: signature[:noncestr], # 必填，生成签名的随机串
       signature: signature[:signature],# 必填，签名，见附录1
-      jsApiList: ['chooseWXPay', 'onMenuShareTimeline'], # 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+      # jsApiList: ['chooseWXPay', 'onMenuShareTimeline'], # 必填，需要使用的JS接口列表，所有JS接口列表见附录2
       expired_at: Wechat.api.jsapi_ticket.got_ticket_at + Wechat.api.jsapi_ticket.ticket_life_in_seconds
     }
   end
