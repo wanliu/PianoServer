ef4c75dcfde39434e8fd57313bc532f267346586
liulei <satbirdd@gmail.com>
2016-07-01 10:08:06 +0800

    生产模式下才必须加载微信ca证书

--- config/initializers/wx_pay.rb
+++ config/initializers/wx_pay.rb
@@ -5,7 +5,15 @@ WxPay.key = wechat_config["token"]
 WxPay.mch_id = wechat_config["mch_id"]
 
 ssl_p12_path = Rails.application.config_for(:wechat)["ssl_p12_path"]
-pass = Rails.application.config_for(:wechat)["ssl_p12_password"]
-WxPay.set_apiclient_by_pkcs12(File.read(ssl_p12_path), pass)
+
+if !Rails.env.production?
+  if ssl_p12_path.present? && File.exist?(ssl_p12_path)
+    pass = Rails.application.config_for(:wechat)["ssl_p12_password"]
+    WxPay.set_apiclient_by_pkcs12(File.read(ssl_p12_path), pass)
+  end
+else
+  pass = Rails.application.config_for(:wechat)["ssl_p12_password"]
+  WxPay.set_apiclient_by_pkcs12(File.read(ssl_p12_path), pass)
+end
 
 WxPay.extra_rest_client_options = {timeout: 1000, open_timeout: 3}
