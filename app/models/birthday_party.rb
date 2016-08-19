4e3d9cc3e606966b7e74027c3d15dcaf44582eca
liulei <satbirdd@gmail.com>
2016-08-19 18:50:06 +0800

    fix cake delete and redpack cant send

--- app/models/birthday_party.rb
+++ app/models/birthday_party.rb
@@ -19,12 +19,14 @@ class BirthdayParty < ActiveRecord::Base
   has_many :blesses
   has_many :redpacks, autosave: true, inverse_of: :birthday_party
 
-  validates :cake, presence: true
+  # validates :cake, presence: true
   validates :user, presence: true
   validates :order, presence: true
   validates :message, presence: true
   validates :birthday_person, presence: true
   validates :hearts_limit, numericality: { greater_than_or_equal_to: 1 }
+
+  # validate :cake_presence
 
   before_validation :set_hearts_limit_from_cake, on: :create
 
@@ -145,4 +147,10 @@ class BirthdayParty < ActiveRecord::Base
     redpacks.where("status = :failed OR status = :unknown", options)
       .map(&:send_redpack)
   end
+
+  # def cake_presence
+  #   unless Cake.with_deleted.find(cake_id).present?
+  #     errors.add(:base, "生日蛋糕不存在")
+  #   end
+  # end
 end
