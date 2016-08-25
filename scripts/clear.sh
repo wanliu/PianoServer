10a7caa688ddcee0fa9453247b46bb26702ddf98
Hysios Hu <hysios@gmail.com>
2016-08-25 13:42:37 +0800

    add clear sh

--- scripts/clear.sh
+++ scripts/clear.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 ASSETS_DIR=$TARGET_DIR/public/assets
-if [ -d "$ASSETS_DIR" ]; then
 
+echo "$ASSETS_DIR"
+
+if [ -d "$ASSETS_DIR" ]; then
+  echo "DELETE $ASSETS_DIR"
   rm -rf $ASSETS_DIR
 fi
