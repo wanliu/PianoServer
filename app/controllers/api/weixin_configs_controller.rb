841d7654a4bf11117c74174c9f3cf7edbede00de
liulei <satbirdd@gmail.com>
2016-09-22 13:50:32 +0800

    eliminate api_ticket from front response

--- app/controllers/api/weixin_configs_controller.rb
+++ app/controllers/api/weixin_configs_controller.rb
@@ -13,4 +13,16 @@ class Api::WeixinConfigsController < Api::BaseController
       expired_at: Wechat.api.jsapi_ticket.got_ticket_at + Wechat.api.jsapi_ticket.ticket_life_in_seconds
     }
   end
+  alias :signature :index
+
+  def card_ext
+    options = {}
+
+    options[:openid] = params[:open_id] if params[:open_id].present?
+    options[:code] = params[:code] if params[:code].present?
+
+    result = Wechat.api.card_api_ticket.card_ext(options)
+
+    render json: {card_ext: result}
+  end
 end
