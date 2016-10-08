fb8a2ff63aa5edfef556cb7cd1004ee1da4174e4
liulei <satbirdd@gmail.com>
2016-10-08 13:56:04 +0800

    record user wx card

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160929090842) do
+ActiveRecord::Schema.define(version: 20161008024936) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -782,6 +782,18 @@ ActiveRecord::Schema.define(version: 20160929090842) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "user_cards", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "card_id"
+    t.string   "encrypt_code"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "user_cards", ["card_id"], name: "index_user_cards_on_card_id", using: :btree
+  add_index "user_cards", ["encrypt_code"], name: "index_user_cards_on_encrypt_code", using: :btree
+  add_index "user_cards", ["user_id"], name: "index_user_cards_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
---
+++ spec/factories/user_cards.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :user_card do
+    user nil
+    card nil
+  end
+end
