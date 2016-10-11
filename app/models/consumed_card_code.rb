74a054ce5dd050dcd8805a01659ec1781aa0e01c
liulei <satbirdd@gmail.com>
2016-10-11 14:27:52 +0800

    store consumed card code record anyway

--- app/models/consumed_card_code.rb
+++ app/models/consumed_card_code.rb
@@ -2,7 +2,7 @@ class ConsumedCardCode < ActiveRecord::Base
   belongs_to :user
   belongs_to :order
 
-  validates :user, presence: true
-  validates :code, presence: true
-  validates :wx_card_id, presence: true
+  # validates :user, presence: true
+  # validates :code, presence: true
+  # validates :wx_card_id, presence: true
 end
